11fcc559b3e888c29e9660dbe1ed99149e6a723d
Orel Hamamy <orelhamamy@gmail.com>
2021-02-17 13:53:49 +0200

    First commit

---
+++ build/simulation_ws/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lab/orel_ws/project/src/simulation_ws
+# Build directory: /home/lab/orel_ws/project/build/simulation_ws
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
