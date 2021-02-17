62cf7fdc83c457bb7b3f9ba992a69e3efcb1416d
Orelhamamy <69792870+Orelhamamy@users.noreply.github.com>
2021-02-17 13:39:23 +0200

    Delete simulation_ws directory

--- simulation_ws/build/gazebo_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lab/orel_ws/project/simulation_ws/src/gazebo_demo
-# Build directory: /home/lab/orel_ws/project/simulation_ws/build/gazebo_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
