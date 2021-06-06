b2fa3f56047b5b0e66bdff11004677d2f35a1250
Orel Hamamy <orelhamamy@gmail.com>
2021-06-06 14:35:39 +0300

    Generate more features visualization
    Also tuning the parameters of the script for high quality figures.

--- model_training/feature_visualization.py
+++ model_training/feature_visualization.py
@@ -28,11 +28,9 @@ def generate_image(imgs, input_size, save =False):
     else:
         plt.savefig(save)
 
-def plot_2D(data_set_path, model_name):
-    model_name = '--'
+def plot_2D(test_set_path, model_name, inx):
     output_layers = [1, 2, 3, 4]
-    data = load_data(data_set_path)
-    inx = 10
+    data = load_data(test_set_path)
     
     try: 
         visual_gen = tf.keras.models.load_model(model_name+'/generator')
@@ -42,44 +40,52 @@ def plot_2D(data_set_path, model_name):
     visual_gen = tf.keras.models.Model(inputs = visual_gen.inputs, outputs = outputs)
     input_size = visual_gen.inputs[0].shape[3]
     f_maps = visual_gen.predict(data[tf.newaxis, :,:,inx:inx+input_size])
-    for feature_map in f_maps:
-        dim = int(np.sqrt(feature_map.shape[3]))
+    for layer, feature_map in enumerate(f_maps):
+        dim = np.int(np.ceil(np.sqrt(feature_map.shape[3])))
         ix = 1
-        plt.figure()
-        for _ in range(dim): # columns
-            for _ in range(dim): # rows
-                ax = plt.subplot(dim, dim, ix)
-                ax.axis('off')
-                plt.imshow(feature_map[0,:,:,ix-1], cmap= 'gray')
-                ix += 1
+        img_dim = feature_map.shape[1]
+        dpi = 1200
+        plt.figure(figsize=(dim*img_dim*1.1/dpi,dim*img_dim*1.1/dpi), dpi=dpi)
+        while(ix<=feature_map.shape[3]):
+            ax = plt.subplot(dim, dim, ix)
+            ax.axis('off')
+            plt.imshow(feature_map[0,:,:,ix-1], cmap= 'gray')
+            ix += 1
+        
+        plt.subplots_adjust(top = 1, bottom = 0, left = 0, right =1 ,wspace= .05, hspace = .05)
+        # plt.tight_layout()
         plt.show()
+        plt.savefig('{}/{} feature-{}.png'.format(model_name, model_name, layer+1), dpi=dpi)
+        plt.savefig('{}/{} feature-{}.esp'.format(model_name, model_name, layer+1), dpi=dpi, format='eps',pad_inches=0.0)
     plt.figure()
     generate_image(data[:,:,inx:inx+input_size], input_size)
 
 
-def plot_3D_conv(test_set_path, model_name):
+def plot_3D_conv(test_set_path, model_name, inx):
     model = Three_d_conv_model(model_name =model_name,data_set_path = test_set_path,
-                               load_model = True)
+                                load_model = True)
     output_layers = [1, 2, 3, 4]
-    inx = 169
     outputs = [model.generator.layers[i].output for i in output_layers]
     visual_gen = tf.keras.models.Model(inputs = model.generator.inputs, outputs = outputs)
     f_maps = visual_gen.predict(model.train_sequence[tf.newaxis, :,:,inx:inx+model.OBSERVE_SIZE, tf.newaxis])
     row = model.OBSERVE_SIZE
     for layer, feature_map in enumerate(f_maps):
         col = feature_map.shape[-1]
-        ix = 1
-        plt.figure()
-        gs1 = gridspec.GridSpec(row, col)
-        gs1.update(wspace = 0.01, hspace = 0.01)
+        img_dim = feature_map.shape[1]
+        dpi = 1200
+        plt.figure(figsize=(col*img_dim*1.2/dpi,row*img_dim*1.3/dpi), dpi=dpi)
+        # gs1 = gridspec.GridSpec(row, col)
+        # gs1.update(wspace = 0.01, hspace = 0.01)
         for j in range(col): # columns
             for i in range(row): # rows
                 ax = plt.subplot2grid((row, col),(i,j))
                 ax.axis('off')
                 plt.imshow(feature_map[0,:,:,i,j], cmap= 'gray')
-                ix += 1
         plt.show()
-        plt.savefig('{}/feature-{}.png'.format(model_name, layer+1), bbox_inches='tight')
+        # plt.tight_layout()
+        plt.subplots_adjust(top = 1, bottom = 0, left = 0, right =1 ,wspace= .05, hspace = .05)
+        plt.savefig('{}/{} feature-{}.png'.format(model_name, model_name, layer+1), dpi=dpi)
+        plt.savefig('{}/{} feature-{}.esp'.format(model_name, model_name, layer+1), dpi=dpi, format='eps')
     plt.figure()
     model.generate_images(inx, model.generator, save = False)
 
@@ -87,5 +93,5 @@ def plot_3D_conv(test_set_path, model_name):
 if __name__ =='__main__':
     test_set_path = '/home/lab/orel_ws/project/data_set_armadillo/2/'
     model_name =  'ARM-3D_conv'
-    plot_3D_conv(test_set_path, model_name)
-    
+    # plot_3D_conv(test_set_path, model_name, 196)
+    # plot_2D('/home/lab/orel_ws/project/data_set/test/','SM-Recursive',178)
